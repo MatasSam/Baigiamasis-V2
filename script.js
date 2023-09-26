15f0a1150017b72802d2fb2523400bbd57b80171
MatasSam <samusis.matas@gmail.com>
2023-09-26 15:59:20 +0300

    update2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict"
+
+var tabs = document.getElementsByClassName("tab")
+
+function showTab(id){
+
+    for(var i = 0; i < tabs.length; i++){
+        tabs[i].style.display = "none"
+    }
+
+    document.getElementById(id).style.display = "block"
+}
--- swiper.js
+++ swiper.js
@@ -6,11 +6,5 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
     }
   });
