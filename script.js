879252a947ce233e06412578ac700d749e241816
MatasSam <samusis.matas@gmail.com>
2023-09-27 11:47:12 +0300

    update3

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 "use strict"
 
+const burger = document.getElementById("burger")
+const links = document.querySelector(".links")
+const anchors = links.querySelectorAll("a")
+
+burger.addEventListener("click", function(){
+    burger.classList.toggle("rotate")
+    links.classList.toggle("showNav")
+})
+
+for(const anchor of anchors){
+    anchor.addEventListener("click", function(){
+        burger.classList.remove("rotate")
+        links.classList.remove("showNav")
+    })
+}
+
+
+
 var tabs = document.getElementsByClassName("tab")
+
+for(var i = 0; i < tabs.length; i++){
+    tabs[i].style.display = "none"
+}
+
+document.getElementById('pirmas').style.display = "block"
 
 function showTab(id){
 
