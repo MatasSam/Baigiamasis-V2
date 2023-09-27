5e8d971f92de0a4eeb6a442ebc86e6351bf3dc16
MatasSam <samusis.matas@gmail.com>
2023-09-27 16:00:08 +0300

    update4

--- swiper.js
+++ swiper.js
@@ -6,5 +6,22 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      }, 
+
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      }
     }
   });
